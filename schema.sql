ad9db58fdf11711972a381fff3d9be8af7c2bff9
hilow2341 <60238749+hilow2341@users.noreply.github.com>
2020-07-15 19:58:15 -0400

    Added Employee Table

--- schema.sql
+++ schema.sql
@@ -20,3 +20,15 @@ CREATE TABLE role (
     PRIMARY KEY (id),
     FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
+
+----- Create Employee Table -----
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE CASCADE
+);
