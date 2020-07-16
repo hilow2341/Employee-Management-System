1320c66e7a7c5f4f920cb4dfc1c1aa2cc95800a6
hilow2341 <60238749+hilow2341@users.noreply.github.com>
2020-07-15 20:08:59 -0400

    Added Role seeds

--- seeds.sql
+++ seeds.sql
@@ -13,3 +13,32 @@ VALUES (3, "Human Resouces");
 
 INSERT INTO department (id, name)
 VALUES (4, "IT");
+
+----- Role Seeds -----
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (1, "Stylist", 42000, 1);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (2, "Assistant Store Manager", 60000, 1);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (3, "Store Manager", 75000, 1);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (4, "Brand Manager", 65000, 2);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (5, "Business Partner", 70000, 3);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (6, "HR Director", 100000, 3);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (7, "System Adminstrator", 80000, 4);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (8, "IT Coordinator", 52000, 4);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (9, "IT Manager", 105000, 4);
